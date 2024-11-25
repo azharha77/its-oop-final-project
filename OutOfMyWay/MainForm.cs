a7d4893cbfd563153195832a490aedc5f40fc755
azharha77 <azharhafiza.77@gmail.com>
2024-11-25 13:20:12 +0700

    inisialisasi

---
+++ OutOfMyWay/MainForm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace OutOfMyWay
+{
+    public class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeForm();
+        }
+
+        private void InitializeForm()
+        {
+            this.Text = "Main Menu";
+            this.Size = new Size(400, 300);
+            this.StartPosition = FormStartPosition.CenterScreen;
+        }
+    }
+}
